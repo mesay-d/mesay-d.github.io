0ac1fe840b69128986b81c661e7184efcefba110
mesay-d <79061934+mesay-d@users.noreply.github.com>
2021-03-23 18:29:05 -0500

    week1day2Assignment

--- cs303/check.js
+++ cs303/check.js
@@ -35,24 +35,92 @@
 // }
 // console.log(reverseArray(["m","h","l","a","c"]))
 
-function scoreExams(array,correctAnswers){
-    let score=[];
-for (let i = 0; i < array.length; i++) {
-  score.push(compare(array[i],correctAnswers))
+// function scoreExams(array,correctAnswers){
+//     let score=[];
+// for (let i = 0; i < array.length; i++) {
+//   score.push(compare(array[i],correctAnswers))
+// }
+
+// return score;
+// }
+// function compare(arr,answer){
+//     let count=0;
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]==answer[i]){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// console.log(compare([1,2,3,4],[1,2,3]))
+// const studentAnswers = [[1, 1, 2], [2, 1, 2], [3, 1, 3]];
+// const correctAnswers = [3, 1, 2];
+// console.log(scoreExams(studentAnswers, correctAnswers))
+
+// let age = 10;
+//  console.log(age);
+//  function output() { 
+//      let age = 20; 
+//     console.log(age);
+// }
+//      output();
+//       console.log(age);
+
+
+const student1={studentid:"s101",quizeAnswer:[1,1,2,4]}
+const student2={studentid:"s102",quizeAnswer:[2,1,2,2]}
+const student3={studentid:"s103",quizeAnswer:[3,1,3,4]}
+const key=[3,1,2,4]
+const quize=[];
+quize.push(student1);
+quize.push(student2);
+quize.push(student3);
+function gradeQuiz(obj,answer){
+    for(const n in obj){
+
+    }
+
 }
 
-return score;
-}
-function compare(arr,answer){
-    let count=0;
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]==answer[i]){
-            count++;
-        }
-    }
-    return count;
-}
-console.log(compare([1,2,3,4],[1,2,3]))
-const studentAnswers = [[1, 1, 2], [2, 1, 2], [3, 1, 3]];
-const correctAnswers = [3, 1, 2];
-console.log(scoreExams(studentAnswers, correctAnswers))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
